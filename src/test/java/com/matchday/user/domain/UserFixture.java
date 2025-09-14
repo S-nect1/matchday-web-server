f60a2423511dfbbb7873a312842041b9e77e30fa
hojooo <ghwn5833@gmail.com>
2025-09-14 12:04:56 +0900

    feat: 사용자 테스트 픽스처 추가
    - UserFixture 클래스에 테스트용 기본 사용자 생성 메서드 추가
    - defaultUser1, defaultUser2 메서드 구현

--- src/test/java/com/matchday/user/domain/UserFixture.java
+++ src/test/java/com/matchday/user/domain/UserFixture.java
@@ -1,4 +1,33 @@
 package com.matchday.user.domain;
 
+import com.matchday.global.entity.enums.City;
+import com.matchday.global.entity.enums.District;
+import com.matchday.team.domain.enums.Position;
+import com.matchday.user.domain.enums.Gender;
+import com.matchday.user.domain.enums.UserRole;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.time.LocalDate;
+
 public class UserFixture {
+    public static User defaultUser1() {
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();;
+
+        return User.createUser("test@example.com", "password", "테스트유저",
+                LocalDate.of(1990, 1, 1), 180, Gender.MALE,
+                Position.MF, UserRole.ROLE_MEMBER, "010-1234-5678",
+                City.SEOUL, District.SEOUL_GANGNAM, false, passwordEncoder);
+
+    }
+
+    public static User defaultUser2() {
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();;
+
+        return User.createUser("test2@example.com", "password2", "테스트유저2",
+                LocalDate.of(1999, 1, 1), 177, Gender.MALE,
+                Position.MF, UserRole.ROLE_MEMBER, "010-4934-5678",
+                City.SEOUL, District.SEOUL_DOBONG, true, passwordEncoder);
+
+    }
 }
