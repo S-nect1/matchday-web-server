32bea2a154b6d6303be24c68e4ec94a93c3e73aa
hojooo <ghwn5833@gmail.com>
2025-09-14 22:50:28 +0900

    fix: endpoint 중복 prefix 수정

--- src/main/java/com/matchday/modules/team/controller/TeamUserController.java
+++ src/main/java/com/matchday/modules/team/controller/TeamUserController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/teams")
+@RequestMapping("/teams")
 @RequiredArgsConstructor
 public class TeamUserController implements TeamUserControllerDocs {
     
